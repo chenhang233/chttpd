28ea3e052e0c6e7ca7af3669278d8c8b96b66a3d
chenhang233 <1973433180@qq.com>
2023-11-27 23:09:30 +0800

    d1: 01

--- demo1/code/01/server.cpp
+++ demo1/code/01/server.cpp
@@ -1,8 +1,23 @@
 #include <sys/socket.h>
-
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(8888);
+
+    bind(sockfd, (sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    listen(sockfd, SOMAXCONN);
+
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_len = sizeof(clnt_addr);
+    bzero(&clnt_addr, sizeof(clnt_addr));
     return 0;
 }
