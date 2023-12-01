b210e6ca62a657374706539787361948c4b6bdd7
chenhang233 <1973433180@qq.com>
2023-12-01 20:10:27 +0800

    feat: 10 thread pool

---
+++ demo1/code/10/src/InetAddress.h
@@ -0,0 +1,15 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#pragma once
+
+class InetAddress
+{
+    // private:
+public:
+    struct sockaddr_in addr;
+    socklen_t addr_len;
+    InetAddress();
+    ~InetAddress();
+    InetAddress(const char *ip, uint16_t port);
+};
---
+++ demo1/code/10/src/Server.cpp
@@ -0,0 +1,33 @@
+#include "Server.h"
+#include "Socket.h"
+#include "Channel.h"
+#include "Acceptor.h"
+#include "Connection.h"
+
+#define READ_BUFFER 1024
+
+Server::Server(EventLoop *_loop) : loop(_loop), acceptor(nullptr)
+{
+    acceptor = new Acceptor(loop);
+    std::function<void(Socket *)> cb = std::bind(&Server::newConnection, this, std::placeholders::_1);
+    acceptor->setNewConnectionCallback(cb);
+}
+
+Server::~Server()
+{
+}
+
+void Server::newConnection(Socket *sock)
+{
+    Connection *conn = new Connection(loop, sock);
+    std::function<void(Socket *)> cb = std::bind(&Server::deleteConnection, this, std::placeholders::_1);
+    conn->setDeleteConnectionCallback(cb);
+    connections[sock->getFd()] = conn;
+}
+
+void Server::deleteConnection(Socket *sock)
+{
+    Connection *conn = connections[sock->getFd()];
+    connections.erase(sock->getFd());
+    delete conn;
+}
