cea5501995d8dc621a00752bd77f1eb035bf08b4
chenhang233 <1973433180@qq.com>
2023-12-02 10:20:17 +0800

    feat: Perfect Forwarding std::future

---
+++ demo1/code/11/src/Epoll.cpp
@@ -0,0 +1,64 @@
+#include "Epoll.h"
+#include <unistd.h>
+
+#define MAX_EVENTS 1000
+
+Epoll::Epoll() : epfd(-1), events(nullptr)
+{
+    errif((epfd = epoll_create1(0)) == -1, "epoll create error");
+    events = new struct epoll_event[MAX_EVENTS];
+    bzero(events, sizeof(*events) * MAX_EVENTS);
+}
+
+Epoll::~Epoll()
+{
+    if (epfd != -1)
+    {
+        close(epfd);
+        epfd = -1;
+    }
+    delete[] events;
+}
+
+void Epoll::addFd(int fd, uint32_t op)
+{
+    struct epoll_event e;
+    bzero(&e, sizeof(e));
+    e.data.fd = fd;
+    e.events = op;
+    errif(epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &e) == -1, "epoll add event error");
+}
+
+std::vector<Channel *> Epoll::poll(int timeout)
+{
+    std::vector<Channel *> activeChannels;
+    int nfds = epoll_wait(epfd, events, MAX_EVENTS, timeout);
+    errif(nfds == -1, "epoll wait error");
+    for (int i = 0; i < nfds; ++i)
+    {
+        Channel *ch = (Channel *)events[i].data.ptr;
+        ch->setRevents(events[i].events);
+        activeChannels.push_back(ch);
+    }
+    return activeChannels;
+}
+
+void Epoll::updateChannel(Channel *channel)
+{
+    int fd = channel->getFd();
+    struct epoll_event ev;
+    bzero(&ev, sizeof(ev));
+    ev.data.ptr = channel;
+    ev.events = channel->getEvents();
+    if (!channel->getInEpoll())
+    {
+        errif(epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) == -1, "epoll add error");
+        channel->setInEpoll();
+        // debug("Epoll: add Channel to epoll tree success, the Channel's fd is: ", fd);
+    }
+    else
+    {
+        errif(epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev) == -1, "epoll modify error");
+        // debug("Epoll: modify Channel in epoll tree success, the Channel's fd is: ", fd);
+    }
+}
